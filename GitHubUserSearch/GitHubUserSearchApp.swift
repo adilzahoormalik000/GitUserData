78e285b214ebbf1b60c5a636047fdabf8a5404f4
ADIL ZAHOOR MALIK <adilzahoormalik@gmail.com>
2024-11-19 11:31:52 +0100

    Initial Commit

---
+++ GitHubUserSearch/GitHubUserSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserSearchApp.swift
+//  GitHubUserSearch
+//
+//  Created by ADIL ZAHOOR MALIK on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
